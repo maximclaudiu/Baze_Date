deb51f03188b333154696d7fa336211bf55b6a2a
maximclaudiu <60611723+maximclaudiu@users.noreply.github.com>
2021-01-05 18:13:49 +0200

    test adaugare

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
 ?>
 <div class="header">
 	<div class="navbar">
-		<a class="nav-link" href="Pages/Employees.php">Employees</a>
-        <a class="nav-link" href="Pages/Jobs.php">Jobs</a>
-        <a class="nav-link" href="Pages/Cities.php">Cities</a>
-        <a class="nav-link" href="Pages/Students.php">Students</a>
-        <a class="nav-link" href="Pages/Grades.php">Grades</a>
+		<a class="nav-link" href="Pages/employees.php">Employees</a>
+        <a class="nav-link" href="Pages/jobs.php">Jobs</a>
+        <a class="nav-link" href="Pages/cities.php">Cities</a>
+        <a class="nav-link" href="Pages/students.php">Students</a>
+        <a class="nav-link" href="Pages/grades.php">Grades</a>
 	</div>
 </div>
 
